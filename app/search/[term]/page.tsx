5663ed2cd22a320295b9441be601abb7e111b427
Micah Bugbee <micahbugbee@hotmail.com>
2023-12-05 08:49:56 -0600

    final build complete

---
+++ app/search/[term]/page.tsx
@@ -0,0 +1,36 @@
+import ResultsList from "@/components/ResultsList";
+import { getFetchUrl } from "@/lib/getFetchUrl";
+import { PageResult, SearchParams } from "@/typings";
+import { redirect } from "next/navigation";
+
+export const revalidate = 120;
+
+type Props = {
+    searchParams: SearchParams;
+    params: {
+        term: string;
+    };
+};
+
+async function SearchPage({ searchParams, params: { term } }: Props) {
+    if (!term) {
+        redirect("/")
+    }
+
+    const response = await fetch(getFetchUrl('api/search'), {
+        method: 'POST',
+        body: JSON.stringify({ searchTerm: term, ...searchParams }),
+    })
+
+    const results = await response.json() as PageResult[];
+
+    console.log(results);
+
+  return (
+    <div>
+        <ResultsList results={results} term={term} />
+    </div>
+  )
+}
+
+export default SearchPage;
